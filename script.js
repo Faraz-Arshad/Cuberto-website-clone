e5b64851b5731d052e4f3d1580453e55de1ace57
Faraz <137032213+Faraz-Arshad@users.noreply.github.com>
2025-06-03 04:45:39 +0500

    Latest with newer cdns

--- script.js
+++ script.js
@@ -48,6 +48,36 @@ function homeVideo (){
             });
         });
     });
-    
 } 
 homeVideo()
+
+
+gsap.to (".fleftelm", {
+    scrollTrigger: {
+        trigger: "#fimages",
+        pin: true,
+        start: ".start top",
+        end: "bottom bottom",
+        endTrigger: ".last",
+        scrub: 1,
+    },
+    y: "-300%",
+    ease: Power1
+})
+
+let sections = document.querySelectorAll(".fleftelm");
+Shery.imageEffect(".images", {
+    style: 5,
+    slideStyle: (setScroll) => {
+       sections.forEach(function(section, index){
+        ScrollTrigger.create({
+            trigger: section,
+            start: "top top",
+            scrub: 1,
+            onUpdate: function(prog){
+               setScroll(prog.progress+index)
+            }
+        })
+       })
+    },
+  }); 
